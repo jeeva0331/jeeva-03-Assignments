efa891ceb85b9795aadf813f8f1367375f46c38b
Jeeva <mjeeva7695@gmail.com>
2026-03-05 11:44:08 +0530

    Create withdrawalamount.java

---
+++ withdrawalamount.java
@@ -0,0 +1,44 @@
+/*Create a simple ATM program:
+Initial balance = 10000
+
+Ask user to enter withdrawal amount
+
+If amount > balance → Print "Insufficient Balance"
+
+Else if amount <= 0 → Print "Invalid Amount"
+
+Else → Deduct amount and print remaining balance*/
+
+
+
+
+
+package com.index.pack;
+
+import java.util.Scanner;
+public class Atm {
+	public static void main(String [] args) {
+	
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the withdrawal amount");
+		int amount=sc.nextInt();
+		
+		int balance=10000;
+		if(amount>balance) {
+			System.out.println("insufficient balance");
+			
+		}
+		else if(amount<=0) {
+			System.out.println("invalid amount");
+
+		}
+		else {
+			System.out.println("deduct money from your account");
+			System.out.println("Remaining balance is "+(balance-amount));
+
+
+		}
+		
+	}
+
+}
