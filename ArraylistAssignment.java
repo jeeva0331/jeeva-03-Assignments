e369b0e93f9e700304dab5f9a8cb418ff582e529
Jeeva <mjeeva7695@gmail.com>
2026-04-12 14:12:48 +0530

    Create ArraylistAssignment.java

---
+++ ArraylistAssignment.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Test {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>(
+            Arrays.asList(1, 2, 3, 4, 5)
+        );
+
+        int k = 1;
+
+        for (int i = 0; i < k; i++) {
+            int last = list.remove(list.size() - 1);
+            list.add(0, last);
+        }
+
+        System.out.println(list); // [4, 5, 1, 2, 3]
+    }
+}
