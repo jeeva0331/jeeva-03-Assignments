b9449815c5561570f55bdb8e3754599f9a568790
Jeeva <mjeeva7695@gmail.com>
2026-05-04 23:32:51 +0530

    Create JDBCAssignment2.java

---
+++ JDBCAssignment2.java
@@ -0,0 +1,33 @@
+package project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JDBCAssignment2 {
+
+    static final String DB_URL = "jdbc:mysql://localhost:3306/jeeva";
+    static final String USER = "root";
+    static final String PASS = "admin@123";
+
+    public static void main(String[] args) {
+
+        try (
+            Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
+            PreparedStatement ps = con.prepareStatement(
+                "UPDATE student SET sname=? WHERE sid=?"
+            );
+        ) {
+
+            ps.setString(1, "Mani");
+            ps.setInt(2, 110);
+
+            int i = ps.executeUpdate();
+            System.out.println(i + " record updated");
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+}
