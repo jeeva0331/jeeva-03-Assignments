2929478effe71920ca450bd95683eb749417a252
Jeeva <mjeeva7695@gmail.com>
2026-05-07 19:33:33 +0530

    Add string reversal functionality in Main class

---
+++ StringHandling3.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String str = "Hello";
+        String rev = "";
+
+        // Reverse string
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        System.out.println("Reversed String: " + rev);
+    }
+}
